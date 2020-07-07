bce917929b464226856985b06134022edde98c45
sam@freighttrust.com <sam@freighttrust.com>
2020-07-07 14:04:19 -0700

    init

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'CommodityFX',
+    shortName: 'CommotidtyFX Exchange',
+    description: 'Stablecoins, Governance Tokens, and Options',
+  },
+  plugins: ['@primer/gatsby-theme-doctocat'],
+}
