21f5005fd42161d439f6eeb39805f8d1602ffe88
sam@freighttrust.com <sam@freighttrust.com>
2020-07-07 17:23:21 -0700

    style(fixing styling and logo changes): fixing logo and theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: 'CommodityFX',
-    shortName: 'CommotidtyFX Exchange',
-    description: 'Stablecoins, Governance Tokens, and Options',
+    title: 'Drewry Container Benchmark Index',
+    shortName: 'Documentation',
+    description: 'Composable Benchmark Container Rate Index',
   },
   plugins: ['@primer/gatsby-theme-doctocat'],
+    resolve: '@primer/gatsby-theme-doctocat',
+      options: {
+        icon: './src/images/icon.png', // This path is relative to the root of the site.
 }
---
+++ src/@primer/gatsby-theme-doctocat/components/hero.js
@@ -0,0 +1,9 @@
+// src/@primer/gatsby-theme-doctocat/components/hero.js
+import {Box} from '@primer/components'
+export default function Hero() {
+  return (
+    <Box bg="black" p={5}>
+      Drewry Container Rate Index
+    </Box>
+  )
+}
